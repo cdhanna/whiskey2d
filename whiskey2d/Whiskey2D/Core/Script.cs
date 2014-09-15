9afe1934cff9ac978b3665eb91fc4b631927ab60
cdhanna <cdhanna@wpi.edu>
2014-09-15 09:00:35 -0400

    adds comments to Script

--- whiskey2d/Whiskey2D/Core/Script.cs
+++ whiskey2d/Whiskey2D/Core/Script.cs
@@ -5,18 +5,28 @@ using System.Text;
 
 namespace Whiskey2D.Core
 {
+
+    /// <summary>
+    /// The base class for all scripts.
+    /// Scripts control GameObject behavour.
+    /// </summary>
     public abstract class Script
     {
 
         private GameObject gob;
 
 
-        
+        /// <summary>
+        /// creates a new Script
+        /// </summary>
         public Script()
         {
             
         }
 
+        /// <summary>
+        /// The GameObject that the script is controlling
+        /// </summary>
         public GameObject Gob
         {
             get
@@ -29,8 +39,14 @@ namespace Whiskey2D.Core
             }
         }
 
-
+        /// <summary>
+        /// Called once upon startup of the GameObject
+        /// </summary>
         public abstract void onStart();
+
+        /// <summary>
+        /// Called upon every update cycle of the GameObject
+        /// </summary>
         public abstract void onUpdate();
 
         
