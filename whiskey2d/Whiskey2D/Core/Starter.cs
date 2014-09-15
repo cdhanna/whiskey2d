b5d440d8c06497ab776e78cd735614db1fc6f786
cdhanna <cdhanna@wpi.edu>
2014-09-15 09:05:37 -0400

    adds comments to Starter

--- whiskey2d/Whiskey2D/Core/Starter.cs
+++ whiskey2d/Whiskey2D/Core/Starter.cs
@@ -5,10 +5,17 @@ using System.Text;
 
 namespace Whiskey2D.Core
 {
+
+    /// <summary>
+    /// A temporary class that a designer must implement to start their game. When the GameManager loads, it will call the first implementation
+    /// of Starter it finds inside a user game.dll. 
+    /// </summary>
     public abstract class Starter
     {
         
-
+        /// <summary>
+        /// Use this method to run startup initilization of the game
+        /// </summary>
         public abstract void start();
     }
 }
