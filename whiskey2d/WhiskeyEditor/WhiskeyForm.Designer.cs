58f382ed0b3aa2144479476133740abd4bb317d9
cdhanna <cdhanna@wpi.edu>
2014-09-15 18:20:09 -0400

    adds basic gui shell

---
+++ whiskey2d/WhiskeyEditor/WhiskeyForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WhiskeyEditor
+{
+    partial class WhiskeyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuBar = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openProjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.directoryTree = new System.Windows.Forms.TreeView();
+            this.openProjectDialog = new System.Windows.Forms.OpenFileDialog();
+            this.debug = new System.Windows.Forms.Label();
+            this.newProjectDialog = new System.Windows.Forms.SaveFileDialog();
+            this.menuBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuBar
+            // 
+            this.menuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuBar.Location = new System.Drawing.Point(0, 0);
+            this.menuBar.Name = "menuBar";
+            this.menuBar.Size = new System.Drawing.Size(922, 28);
+            this.menuBar.TabIndex = 0;
+            this.menuBar.Text = "menuBar";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newProjectToolStripMenuItem,
+            this.openProjectToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newProjectToolStripMenuItem
+            // 
+            this.newProjectToolStripMenuItem.Name = "newProjectToolStripMenuItem";
+            this.newProjectToolStripMenuItem.Size = new System.Drawing.Size(164, 24);
+            this.newProjectToolStripMenuItem.Text = "New Project";
+            this.newProjectToolStripMenuItem.Click += new System.EventHandler(this.newProjectToolStripMenuItem_Click);
+            // 
+            // openProjectToolStripMenuItem
+            // 
+            this.openProjectToolStripMenuItem.Name = "openProjectToolStripMenuItem";
+            this.openProjectToolStripMenuItem.Size = new System.Drawing.Size(164, 24);
+            this.openProjectToolStripMenuItem.Text = "Open Project";
+            this.openProjectToolStripMenuItem.Click += new System.EventHandler(this.openProjectToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(164, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // directoryTree
+            // 
+            this.directoryTree.Location = new System.Drawing.Point(12, 31);
+            this.directoryTree.Name = "directoryTree";
+            this.directoryTree.Size = new System.Drawing.Size(225, 377);
+            this.directoryTree.TabIndex = 1;
+            // 
+            // openProjectDialog
+            // 
+            this.openProjectDialog.FileName = "openProjectDialog";
+            // 
+            // debug
+            // 
+            this.debug.AutoSize = true;
+            this.debug.Location = new System.Drawing.Point(253, 31);
+            this.debug.Name = "debug";
+            this.debug.Size = new System.Drawing.Size(46, 17);
+            this.debug.TabIndex = 2;
+            this.debug.Text = "label1";
+            // 
+            // newProjectDialog
+            // 
+            this.newProjectDialog.CheckPathExists = false;
+            this.newProjectDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.newProjectDialog_FileOk);
+            // 
+            // WhiskeyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 420);
+            this.Controls.Add(this.debug);
+            this.Controls.Add(this.directoryTree);
+            this.Controls.Add(this.menuBar);
+            this.MainMenuStrip = this.menuBar;
+            this.Name = "WhiskeyForm";
+            this.Text = "Whiskey2D Editor";
+            this.menuBar.ResumeLayout(false);
+            this.menuBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuBar;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newProjectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openProjectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TreeView directoryTree;
+        private System.Windows.Forms.OpenFileDialog openProjectDialog;
+        private System.Windows.Forms.Label debug;
+        private System.Windows.Forms.SaveFileDialog newProjectDialog;
+    }
+}
+
---
+++ whiskey2d/WhiskeyEditor/WhiskeyForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhiskeyEditor
+{
+    public partial class WhiskeyForm : Form
+    {
+        public WhiskeyForm()
+        {
+            InitializeComponent();
+
+            //this.newProjectDialog.ex
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void openProjectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            DialogResult result = this.openProjectDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string directory = this.openProjectDialog.InitialDirectory + this.openProjectDialog.FileName;
+                //TODO validate this to make sure its a real Whiskey Game Directory
+            }
+
+          
+
+        }
+
+        private void newProjectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = this.newProjectDialog.ShowDialog();
+            
+            if (result == DialogResult.OK)
+            {
+                
+                Console.WriteLine(newProjectDialog.FileName);
+                //this.debug.Text = this.newProjectDialog.fi;
+            }
+        }
+
+        private void newProjectDialog_FileOk(object sender, CancelEventArgs e)
+        {
+           
+        }
+    }
+}
