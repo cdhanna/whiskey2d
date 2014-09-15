bafc1d3ec024f11caab5cbfe512398d6c1eeee9d
cdhanna <cdhanna@wpi.edu>
2014-09-15 08:54:09 -0400

    adds comments to InputManager

--- whiskey2d/Whiskey2D/Core/InputManager.cs
+++ whiskey2d/Whiskey2D/Core/InputManager.cs
@@ -5,10 +5,19 @@ using System.Text;
 using Microsoft.Xna.Framework.Input;
 namespace Whiskey2D.Core
 {
+
+    /// <summary>
+    /// Manages all input for the game. Only keyboard input is supported
+    /// </summary>
     public class InputManager
     {
 
         private static InputManager instance;
+
+        /// <summary>
+        /// retrieves the InputManager
+        /// </summary>
+        /// <returns>The InputManager</returns>
         public static InputManager getInstance()
         {
             if (instance == null)
@@ -25,15 +34,24 @@ namespace Whiskey2D.Core
         {
         }
 
+        /// <summary>
+        /// Initializes the InputManager
+        /// </summary>
         public void init()
         {
             currentState = Keyboard.GetState();
         }
 
+        /// <summary>
+        /// Closes the InputManager
+        /// </summary>
         public void close()
         {
         }
 
+        /// <summary>
+        /// Updates the InputManager, allowing keyboard events to be heard and registered
+        /// </summary>
         public void update()
         {
             oldState = currentState;
@@ -41,11 +59,21 @@ namespace Whiskey2D.Core
             
         }
 
+        /// <summary>
+        /// Determines if a single key is being pressed at the moment
+        /// </summary>
+        /// <param name="key">A key to check if it is down</param>
+        /// <returns>True if the key is down, false otherwise</returns>
         public Boolean isKeyDown(Keys key)
         {
             return currentState.IsKeyDown(key);
         }
 
+        /// <summary>
+        /// Determines if a single key has been newly pressed. Holding down a key will only trigger this method once.
+        /// </summary>
+        /// <param name="key">A key to check if it is newly down</param>
+        /// <returns>True if the key was just pressed, false otherwise</returns>
         public Boolean isNewKeyDown(Keys key)
         {
             return (currentState.IsKeyDown(key) && oldState.IsKeyUp(key));
