05b94c7c17076348a16950df049738b6fdf6c886
cdhanna <cdhanna@wpi.edu>
2014-09-15 08:46:20 -0400

    adds comments to Bounds

--- whiskey2d/Whiskey2D/Core/Bounds.cs
+++ whiskey2d/Whiskey2D/Core/Bounds.cs
@@ -6,20 +6,51 @@ using Microsoft.Xna.Framework;
 
 namespace Whiskey2D.Core
 {
+
+    /// <summary>
+    /// Simple solution for rectangular collision.
+    /// </summary>
     public class Bounds
     {
 
         private Vector2 position;
         private Vector2 size;
 
+        /// <summary>
+        /// The top-left position of the bounds
+        /// </summary>
         public Vector2 Position { get { return position; } }
+
+        /// <summary>
+        /// The width and height of the bounds, in terms of X and Y 
+        /// </summary>
         public Vector2 Size { get { return size; } }
 
+        /// <summary>
+        /// Tthe bottam Y location of the bound
+        /// </summary>
         public float Bottam { get { return position.Y + size.Y; } }
+
+        /// <summary>
+        /// Tthe top Y location of the bound
+        /// </summary>
         public float Top { get { return position.Y; } }
+
+        /// <summary>
+        /// Tthe right X location of the bound
+        /// </summary>
         public float Right { get { return position.X + size.X; } }
+
+        /// <summary>
+        /// Tthe left X location of the bound
+        /// </summary>
         public float Left { get { return position.X; } }
 
+        /// <summary>
+        /// Create a Bound from a top-left position, and a width-height
+        /// </summary>
+        /// <param name="position">The top-left position of the bounds</param>
+        /// <param name="size">The width-height of the bounds, in terms of X and Y</param>
         public Bounds(Vector2 position, Vector2 size)
         {
             this.position = position;
