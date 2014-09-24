38a2d2b5a80bd6d2ad66ba2f8d40d0b8f267bdf3
cdhanna <cdhanna@wpi.edu>
2014-09-23 21:35:40 -0400

    preps to whack GameManager

---
+++ whiskey2d/Whiskey2D/Core/MonoGame.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+
+namespace Whiskey2D.Core
+{
+
+
+    /// <summary>
+    /// A way to run the GameManager through MonoGame's built in Game
+    /// </summary>
+    public class MonoGame : Game
+    {
+
+
+
+        public MonoGame() : base()
+        {
+
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+
+    }
+}
